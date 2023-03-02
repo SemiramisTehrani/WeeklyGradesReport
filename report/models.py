e7a8c7a4f99b6b5ed03fc489d3d2f5e20a5516cf
unknown <semtehrani@yahoo.com>
2023-03-02 09:39:42 -0800

    created report app , made report class inside Modle.py, database & code is sync

--- report/models.py
+++ report/models.py
@@ -1,3 +1,11 @@
+from pyexpat import model
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+class Report(models.Model) :
+    courses          = models.CharField(max_length = 255)
+    grades           = models.CharField(max_length = 255)
+    date             = models.CharField(max_length = 255)
+    
